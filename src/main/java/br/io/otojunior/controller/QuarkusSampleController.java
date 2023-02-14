3231d7cf5eeaae35f0645f6c4b468fe54c205ede
Oto Soares Coelho Júnior <otojunior@gmail.com>
2023-02-14 14:52:53 -0300

    Logging configurado para Tinylog-Jboss

--- src/main/java/br/io/otojunior/controller/QuarkusSampleController.java
+++ src/main/java/br/io/otojunior/controller/QuarkusSampleController.java
@@ -5,6 +5,8 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import org.tinylog.Logger;
+
 @Path("/hello")
 public class QuarkusSampleController {
     /**
@@ -14,6 +16,12 @@ public class QuarkusSampleController {
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
+    	Logger.error("Mensagem ERRO de exemplo");
+        Logger.warn("Mensagem AVISO de exemplo");
+        Logger.info("Mensagem INFO de exemplo");
+        Logger.debug("Mensagem DEBUG de exemplo");
+        Logger.trace("Mensagem TRACE de exemplo");
+
         return "Hello from RESTEasy Reactive";
     }
 }
