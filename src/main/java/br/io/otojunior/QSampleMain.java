9f62588c4d6133de4df0a6cc9b7862e343233422
Oto Soares Coelho Júnior <otojunior@gmail.com>
2023-02-26 20:52:52 -0300

    NTP Client com linha de comando Windows

--- src/main/java/br/io/otojunior/QSampleMain.java
+++ src/main/java/br/io/otojunior/QSampleMain.java
@@ -21,34 +21,34 @@ import io.quarkus.runtime.annotations.QuarkusMain;
 @QuarkusMain
 public class QSampleMain {
 	private static final String NTP_SERVER = "pool.ntp.br";
-	
+
     /**
      * Método principal
      * @param args Argumentos de linha de comando.
      */
     public static void main(String[] args) throws Exception {
         var client = new NTPUDPClient();
-        
+
         var watchStart = 0L;
         var watchEnd = 0L;
-        
+
         try {
         	final var defaultZoneId = ZoneId.systemDefault();
         	final var inetAddr = InetAddress.getByName(NTP_SERVER);
         	final var fmthora = DateTimeFormatter.ofPattern("HH:mm:ss");
         	final var fmtdata = DateTimeFormatter.ofPattern("dd/MM/yyyy");
         	final var runtime = Runtime.getRuntime();
-        	
+
         	watchStart = System.currentTimeMillis();
-        	
+
             client.open();
             var timeInfo = client.getTime(inetAddr);
-            
+
             var returnTime = timeInfo
         		.getMessage()
         		.getTransmitTimeStamp()
         		.getTime();
-            
+
 			var time = LocalDateTime
 				.ofInstant(Instant
 				.ofEpochMilli(returnTime), defaultZoneId);
