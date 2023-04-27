b611cd8cb0ae390a6a13429757a0d3f5a09c1a7f
Oto Soares Coelho Júnior <oto.coelho-junior@serpro.gov.br>
2023-04-27 18:21:06 -0300

    Remoção de imports não utilizados

--- src/main/java/br/io/otojunior/controller/QuarkusSampleController.java
+++ src/main/java/br/io/otojunior/controller/QuarkusSampleController.java
@@ -2,8 +2,6 @@ package br.io.otojunior.controller;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
 
 import lombok.extern.slf4j.Slf4j;
 
